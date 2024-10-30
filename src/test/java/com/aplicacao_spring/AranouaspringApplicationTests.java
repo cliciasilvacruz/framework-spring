0d9893a22304bacfadfd9dbd3cd1d9b8d484a60c
cliciasilvacruz <117326494+cliciasilvacruz@users.noreply.github.com>
2024-10-29 21:52:30 -0400

    subindo pasta para o repositorio

---
+++ src/test/java/com/aplicacao_spring/AranouaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacao_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AranouaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
